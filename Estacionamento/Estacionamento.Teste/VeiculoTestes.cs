a16baefaef13170f1ea7d9bff0c42fbfc2c50899
igorcamargo1 <rm94553@fiap.com.br>
2023-10-17 15:31:03 -0300

    Documentando os testes com padrão AAA

--- Estacionamento/Estacionamento.Teste/VeiculoTestes.cs
+++ Estacionamento/Estacionamento.Teste/VeiculoTestes.cs
@@ -7,18 +7,22 @@ public class VeiculoTestes
     [Fact]
     public void TesteVeiucloAcelerar()
     {
+        //Arrange
         var veiculo = new Veiculo();
+        //Act
         veiculo.Acelerar(10);
-
+        //Assert
         Assert.Equal(100, veiculo.VelocidadeAtual);
     }
 
     [Fact]
     public void TesteVeiculoFrear()
     {
+        //Arrange
         var veiculo = new Veiculo();
+        //Act
         veiculo.Frear(10);
-
+        //Assert
         Assert.Equal(-150, veiculo.VelocidadeAtual);
     }
 }
